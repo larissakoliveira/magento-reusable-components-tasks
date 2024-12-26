49fcd5edde9e5a4f0b8b28db81ce5f91255eaba0
Larissa Oliveira <oliveir5uwm@gmail.com>
2024-12-26 14:17:45 -0300

    feature: adds Magento Tasks module and theme

---
+++ app/code/Coe/MagentoTasks/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Coe_MagentoTasks',
+    __DIR__
+);
---
+++ app/design/frontend/Coe/magento_tasks_theme/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Coe/magento_tasks_theme',
+    __DIR__
+);
